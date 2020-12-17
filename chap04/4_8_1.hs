860c5d63e2a7cc1fcce1b41728e013104fbc2cc2
INABA Akira <inaba.akira@gmail.com>
2020-12-17 22:57:08 +0900

    4.8 練習問題 3 終了

--- chap04/4_8_1.hs
+++ chap04/4_8_1.hs
@@ -11,3 +11,21 @@ halve x = splitAt ((length x) `div` 2) x
 safetail :: [a] -> [a]
 safetail (_:xs)  = xs
 safetail [] = []
+
+-- myor :: Bool -> Bool -> Bool
+-- True `myor` True = True
+-- True `myor` False = True
+-- False `myor` True = True
+-- False `myor` False = False
+
+-- myor :: Bool -> Bool -> Bool
+-- False `myor` False = False
+-- _ `myor` _ = True
+
+-- myor :: Bool -> Bool -> Bool
+-- False `myor` b = b
+-- True `myor` _ = True
+
+myor :: Bool -> Bool -> Bool
+b `myor` c | b /= c = True
+           | b == c = b
