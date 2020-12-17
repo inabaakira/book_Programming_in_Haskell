557782e52632205aa84f89d167cd4a7cb8dce251
INABA Akira <inaba.akira@gmail.com>
2020-12-17 22:03:14 +0900

    4.8 練習問題 2. b 終了

--- chap04/4_8_1.hs
+++ chap04/4_8_1.hs
@@ -1,5 +1,9 @@
 halve :: [a] -> ([a], [a])
 halve x = splitAt ((length x) `div` 2) x
 
+-- safetail :: [a] -> [a]
+-- safetail x = if null x then [] else tail x
+
 safetail :: [a] -> [a]
-safetail x = if null x then [] else tail x
+safetail x | null x = []
+           | otherwise = tail x
